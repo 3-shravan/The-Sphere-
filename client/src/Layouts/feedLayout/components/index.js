de8ef28aa025010bbd5a13f437d2477a86c6bd85
Sravan kumar <shantanuyadav379@gmail.com>
2025-09-12 12:45:08 +0530

    Delete client/src/Layouts directory
    as it needs to be pushed again with case sensitive naming convention.

--- client/src/Layouts/feedLayout/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Dock } from "./Dock";
-export { default as Header } from "./PhoneHeader";
-export { default as Sidebar } from "./Sidebar";
--- client/src/Layouts/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FeedLayout } from "./feedLayout/Layout";
-export { default as HomePage } from "./feedLayout/HomePage";
-export { default as LandingPage } from "./landingPage/Page";
