058edfddbd53e8d3f773b1dc2b6cd50a56ca1349
3-shravan <shantanuyadav379@gmail.com>
2025-09-17 05:19:09 +0545

    fix forget api url change

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,8 +1,8 @@
 // const isLocalhost = window.location.hostname === "localhost";
-export const API_URL = "http://localhost:8000/api/v1";
-// || `${import.meta.env.VITE_API_URL}/api/v1`;
+export const API_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
+// || "http://localhost:8000/api/v1";
 
 export const CLIENT_URL =
   import.meta.env.VITE_CLIENT_URL || "http://localhost:5173";
 
-// export const MODE = isLocalhost ? "development" : "production";
+export const MODE = isLocalhost ? "development" : "production";
