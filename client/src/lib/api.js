6bc776ebbc7088f9ad8e5616a1aadbd6551bbe38
3-shravan <shantanuyadav379@gmail.com>
2025-09-20 18:45:02 +0530

    User preference feed ..env api fix

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,17 +1,9 @@
 const isLocalhost = window.location.hostname === "localhost";
 export const MODE = isLocalhost ? "development" : "production";
 
-export const BASE_API_URL = isLocalhost
-  ? import.meta.env.VITE_API_URL
-  : "http://localhost:8000";
-
-export const API_URL = isLocalhost
-  ? `${import.meta.env.VITE_API_URL}/api/v1`
-  : "http://localhost:8000/api/v1";
-
-export const CLIENT_URL = isLocalhost
-  ? import.meta.env.VITE_CLIENT_URL
-  : "http://localhost:5173";
+export const BASE_API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
+export const CLIENT_URL = import.meta.env.VITE_CLIENT_URL;
 
 // export const BASE_API_URL = "http://localhost:8000";
 // export const API_URL = "http://localhost:8000/api/v1";
