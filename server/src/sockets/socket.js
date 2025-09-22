f668b19ef3407d5cb64291b4fd9f41384cbf0be7
3-shravan <shantanuyadav379@gmail.com>
2025-09-22 21:47:43 +0530

    hide logs

--- server/src/sockets/socket.js
+++ server/src/sockets/socket.js
@@ -2,6 +2,13 @@ import { Server } from "socket.io";
 
 export let io = null;
 export const onlineUsers = new Map();
+
+const log = (...args) => {
+  if (process.env.NODE_ENV === "production") {
+    console.log(...args);
+  }
+};
+
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
@@ -11,18 +18,18 @@ export const initSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
-    console.log("⚡ User connected:", socket.id);
+    log("⚡ User connected:", socket.id);
 
     socket.on("register", (userId) => {
       onlineUsers.set(userId, socket.id);
-      console.log(`✅ ${userId} is online`);
+      log(`✅ ${userId} is online`);
     });
 
     socket.on("disconnect", () => {
       for (let [uid, sid] of onlineUsers.entries()) {
         if (sid === socket.id) {
           onlineUsers.delete(uid);
-          console.log(`❌ ${uid} went offline`);
+          log(`❌ ${uid} went offline`);
           break;
         }
       }
